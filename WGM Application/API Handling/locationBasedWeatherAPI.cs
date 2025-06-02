5aada00484c0bd0d8169b4f5bc0cc5ee8adeffff
Connor Doran <connor29020@gmail.com>
2025-06-02 17:47:38 -0400

    Implimented location polling, working on adding a location search feature. Added location display

---
+++ WGM Application/API Handling/locationBasedWeatherAPI.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WGM_Application.API_Handling
+{
+    internal class locationBasedWeatherAPI
+    {
+        public static async Task<string> GetTownFromLatLong(double lat, double lon)
+        {
+            string url = $"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "YourAppName"); // Required by Nominatim
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(jsonResponse);
+                    return $"{data["address"]?["town"]}, {data["address"]?["state"]}" ?? "Unknown Location";
+                }
+            }
+            return "Location lookup failed.";
+        }
+
+
+        public void FetchWeatherForLocation(string location)
+        {
+            System.Diagnostics.Trace.WriteLine("Hello world!");
+        }
+    }
+}
--- WGM Application/MainWindow.xaml.cs
+++ WGM Application/MainWindow.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,9 +5,20 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Popups;
+using Newtonsoft.Json.Linq;
+using WGM_Application.API_Handling;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -36,5 +42,62 @@ namespace WGM_Application
         {
             System.Diagnostics.Trace.WriteLine("Opening Template!");
         }
+
+        // This is for the user based location data and everything directly under will be removed
+        private readonly List<string> locations = new List<string>
+{
+    "New York", "Los Angeles", "Chicago", "Houston", "Miami"
+};
+
+        private void OnLocationTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                var filteredLocations = locations.Where(l => l.StartsWith(sender.Text, StringComparison.OrdinalIgnoreCase)).ToList();
+                sender.ItemsSource = filteredLocations;
+            }
+        }
+
+        private void OnLocationChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            sender.Text = args.SelectedItem.ToString();
+        }
+
+        private string location = "";
+        // Poll user location
+        private async void pollLocation(object sender, RoutedEventArgs e)
+    {
+        var geolocator = new Geolocator();
+        Geoposition position = await geolocator.GetGeopositionAsync();
+
+        double latitude = position.Coordinate.Point.Position.Latitude;
+        double longitude = position.Coordinate.Point.Position.Longitude;
+
+        System.Diagnostics.Trace.WriteLine($"User Location: {latitude}, {longitude}");
+
+        location = await locationBasedWeatherAPI.GetTownFromLatLong( latitude, longitude );
+        System.Diagnostics.Trace.WriteLine(location);
+        LocationDisplay.Text = "Location: " + location;
+    }
+
+
+
+    
+        private async void OpenLocationDialog(object sender, RoutedEventArgs e)
+        {
+            ContentDialogResult result = await LocationDialog.ShowAsync();
+
+            if (string.IsNullOrWhiteSpace(LocationInput.Text))
+            {
+                LocationDisplay.Text = "Please enter a valid location.";
+            }
+            else
+            {
+                location = LocationInput.Text;
+                LocationDisplay.Text = "Location: " + location;
+            }
+
+        }
+
     }
 }
