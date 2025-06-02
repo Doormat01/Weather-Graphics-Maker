e9d7e70832a6d7af1cf3403580a6b1d48e8e8b38
Connor Doran <connor29020@gmail.com>
2025-06-02 16:35:46 -0400

    Version 0.0.1 Alpha

---
+++ WGM Application/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WGM_Application
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void openActions(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Hello world!");
+        }
+        public void openTemplate(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Opening Template!");
+        }
+    }
+}
